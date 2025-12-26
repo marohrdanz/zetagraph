f618ff7c50632054469b8bc309956a14f21f748e
Mary Rohrdanz <marohrdanz@mdanderson.org>
2025-12-26 17:26:21 -0600

    Tweak logging format

--- log_setup.py
+++ log_setup.py
@@ -15,19 +15,12 @@ def configure_logging():
             'ERROR': 'red',
             'CRITICAL': 'bold_red'
         }
-        if (level == 'DEBUG'): # use more extensive logging output for development mode
-            formatter = ColoredFormatter(
+        formatter = ColoredFormatter(
               '%(log_color)s%(asctime)s %(levelname)5s%(reset)s - %(light_black)s%(filename)s:%(lineno)d %(funcName)s%(reset)s - %(log_color)s%(message)s%(reset)s',
               datefmt='%H:%M:%S',
               reset=True,
               log_colors=COLORS
-            )
-        else: # use cleaner log format for production
-            formatter = ColoredFormatter(
-              '%(asctime)s - %(levelname)s - %(message)s',
-              datefmt='%Y-%m-%d %H:%M:%S',
-              reset=True
-            )
+        )
         # Stream handler
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
@@ -36,7 +29,7 @@ def configure_logging():
         # File handler
         log_file = getenv('LOG_FILE', 'app.log')
         file_handler = logging.FileHandler(log_file)
-        file_formatter = ColoredFormatter(
+        file_formatter = ColoredFormatter( # no color codes in file
           '%(asctime)s - %(levelname)s - %(message)s',
           datefmt='%Y-%m-%d %H:%M:%S',
           reset=True
